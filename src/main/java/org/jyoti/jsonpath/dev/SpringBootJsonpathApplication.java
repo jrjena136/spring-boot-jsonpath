339e4c4797672d55a6d78d5e8c824f50068982c7
Jyotiranjan Jena <jrjena136@gmail.com>
2018-09-07 15:49:49 +0530

    update
    added some to code to run in tomcat

--- src/main/java/org/jyoti/jsonpath/dev/SpringBootJsonpathApplication.java
+++ src/main/java/org/jyoti/jsonpath/dev/SpringBootJsonpathApplication.java
@@ -2,10 +2,15 @@ package org.jyoti.jsonpath.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootJsonpathApplication {
-
+public class SpringBootJsonpathApplication  extends SpringBootServletInitializer{
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootJsonpathApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJsonpathApplication.class, args);
 	}
